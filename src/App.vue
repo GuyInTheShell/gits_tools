8d4c935d999f4140bd1da44f0b00373d2204cbf9
Grégory Bataille <gregory.bataille@gmail.com>
2023-11-20 13:45:58 +0100

    feat: early scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,56 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="page">
+    <header>
+      You did it!
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div id="content">
+      <nav>
+        Menu
+      </nav>
+
+      <main>
+        Main
+      </main>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
+#page {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  height: 100vh;
+  width: 100vw;
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  >header {
+    min-height: 75px;
+    background: lightgray;
+    line-height: 1.5;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  >#content {
+    flex-grow: 1;
 
-  header .wrapper {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    flex-wrap: nowrap;
+
+    >nav {
+      background: lightblue;
+      width: 200px;
+    }
+
+    >main {
+      flex-grow: 1;
+
+      background: red;
+      width: 100px;
+    }
   }
 }
 </style>
