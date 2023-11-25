c17221e8b83537e9b796bca74f9e2dab3efb6e5e
Grégory Bataille <gregory.bataille@gmail.com>
2023-11-25 08:50:24 +0100

    feat: try some sort of composition

---
+++ src/components/pipeline/transformers/BaseTransformer.vue
@@ -0,0 +1,40 @@
+<script>
+</script>
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  title: string
+  transform: Function
+  input?: string
+}>()
+
+const emit = defineEmits<{
+  valueChange: [text: string]     // broadcasts the new value of the transformer
+}>()
+
+// Used to react to any change of the input to emit the output
+const output = computed(() => {
+  if (props.input === undefined || !props.input) {
+    return ""
+  }
+  var out = props.transform(props.input);
+  emit('valueChange', out);
+  return out;
+})
+
+</script>
+
+<template>
+  <v-card color="#ECE3CE">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-subtitle>bin <v-icon icon="mdi-arrow-right" /> str</v-card-subtitle>
+    <v-card-text v-if="props.input">
+      <p>Input: {{ props.input.substring(0, 10) }}...</p>
+      <!-- the computed property needs to be used if we want the emit it contains to be triggered -->
+      <p>Output: {{ output.substring(0, 10) }}...</p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped></style>
--- src/components/pipeline/transformers/ToBase64.vue
+++ src/components/pipeline/transformers/ToBase64.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import BaseTransformer from './BaseTransformer.vue'
 
 
 const props = defineProps<{
@@ -10,28 +10,15 @@ const emit = defineEmits<{
   valueChange: [text: string]     // broadcasts the new value of the transformer
 }>()
 
-// Used to react to any change of the input to emit the output
-const output = computed(() => {
-  if (props.input === undefined || !props.input) {
-    return ""
-  }
-  var out = btoa(props.input);
-  emit('valueChange', out);
-  return out;
-})
+function transform(payload: string): string {
+  return btoa(payload)
+}
 
 </script>
 
 <template>
-  <v-card color="#ECE3CE">
-    <v-card-title>To Base64</v-card-title>
-    <v-card-subtitle>bin <v-icon icon="mdi-arrow-right" /> str</v-card-subtitle>
-    <v-card-text v-if="props.input">
-      <p>Input: {{ props.input.substring(0, 10) }}...</p>
-      <!-- the computed property needs to be used if we want the emit it contains to be triggered -->
-      <p>Output: {{ output.substring(0, 10) }}...</p>
-    </v-card-text>
-  </v-card>
+  <BaseTransformer :input="props.input" @value-change="(out) => emit('valueChange', out)" title="foobar"
+    :transform="transform" />
 </template>
 
 <style scoped></style>
